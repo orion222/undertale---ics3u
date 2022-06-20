e652eba1c6b0c4c7dc42a2bbbfda27c7e310d714
orion222 <56134554+orion222@users.noreply.github.com>
2022-06-20 00:58:14 -0400

    Add files via upload

--- animation.java
+++ animation.java
@@ -1,3 +1,11 @@
+/*
+ * This class stores the animation methods for the fading animation.
+ * It is used to transition between settings of each map.
+ * Also used for the beginning where you click play and it loads you
+ * into the first map.
+ */
+
+
 import java.awt.*;
 import java.awt.AlphaComposite;
 import java.awt.Graphics;
@@ -27,7 +35,13 @@ public class animation implements ActionListener{
 
 		game = e;
 	}
-
+	
+	
+	/*
+	 * The "father" method of fade.
+	 * The main file will call this file that fades out one image and 
+	 * fades in the other. There is also a speed if you want to fade it quicker.
+	 */
 	public void fade(BufferedImage start, BufferedImage end, String speed) {
 		fading = true;
 		if (speed.equals("fast")) {
@@ -39,50 +53,63 @@ public class animation implements ActionListener{
 		timer.start();
 
 	}
-	public void fadeOut() {
-		alpha -= fadeSpeed;
-		if (alpha < 0) {
-			alpha = 0;
-			faded = true;
-			if (wait) {
-				try {
-					Thread.sleep(3000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			game.mapX = 0;
-			game.mapY = 0;
+	
+	
+	/*
+	 * The method to decrease the opacity of an image.
+	 * It may also pause at a black screen just for dramatic effect.
+	 * It also changes globalPosition to the appropriate value for 
+	 * maps that have the camera move. 
+	 */
+    public void fadeOut() {
+        alpha -= fadeSpeed;
+        if (alpha < 0) {
+            alpha = 0;
+            faded = true;
+            if (wait) {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            game.mapX = 0;
+            game.mapY = 0;
 
-			if (game.gameState == 1 && game.setting == 4 && game.change == 1) {
-				game.mapY = -610;
-				System.out.println("WRONG!!");
-			}
+            if (game.gameState == 1 && game.setting == 4 && game.change == 1) {
+                game.mapY = -610;
+                System.out.println("WRONG!!");
+            }
 
-			else if (game.gameState == 1 && game.setting == 3 && game.change == 2) {
-				game.mapY = -610;
-				System.out.println("HESHEDASG");
-				// when coming back from snowden, map needs to draw at -140
-			}
-			else if (game.gameState == 1 && game.setting == 4 && game.change == 2) {
-				game.mapY = -145;
-				game.globalPos = 240;
-			}
+            else if (game.gameState == 1 && game.setting == 3 && game.change == 2) {
+                game.mapY = -610;
+                System.out.println("HESHEDASG");
+                // when coming back from snowden, map needs to draw at -140
+            }
+            else if (game.gameState == 1 && game.setting == 4 && game.change == 2) {
+                game.mapY = -145;
+                game.globalPos = 240;
+            }
 
-			else if (game.gameState == 2 && game.setting == 1 && game.change == 2) {
-				game.mapX = -830;
-			}
-			else if(game.gameState == 2 && game.setting == 4 && game.change == 1) {
-				game.globalPos = 60;
-			}
-			else if (game.gameState == 2 && game.setting == 4 && game.change == 2) {
-				game.mapX = -670;
-				game.globalPos = 1535;
-			}
-		}
-	}
-
+            else if (game.gameState == 2 && game.setting == 1 && game.change == 2) {
+                game.mapX = -830;
+            }
+            else if(game.gameState == 2 && game.setting == 4 && game.change == 1) {
+                game.globalPos = 60;
+            }
+            else if (game.gameState == 2 && game.setting == 4 && game.change == 2) {
+                game.mapX = -670;
+                game.globalPos = 1535;
+            }
+        }
+    }
+    
+    
+    /*
+     * Method to increase opacity of an image to 100%
+     * Will fade in an image.
+     */
 	public void fadeIn() {
 		alpha += fadeSpeed;
 		if (alpha > 1) {
@@ -92,7 +119,12 @@ public class animation implements ActionListener{
 			timer.stop();
 		}
 	}
-
+	
+	
+	/*
+	 * The method that is called every 20 milliseconds by the timer
+	 * Checks if we are fading out or not, and executes the corresponding methods 
+	 */
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
--- dialogue.java
+++ dialogue.java
@@ -1,3 +1,12 @@
+
+// INTRO COMMENTS
+// the purpose of this file is to integrate text-file streaming
+// most settings will have a script that will play once the player
+// enters that setting. The following has a method to scroll through
+// the dialogue, as we want to display messages 3 lines at a time 
+// to not overload the player
+
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -16,131 +25,149 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 public class dialogue extends JPanel implements KeyListener{
+	
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static BufferedImage dialogueBox;
+	public static Font font;
+	
+	// max letters = 54
+	
+	public static boolean speaking = true;
+	public static Scanner currentFile;
+	public static ArrayList<String>[][] allScripts = new ArrayList[4][5];
+	public static int startLine = 0;
+	public static int maxLines = 0;
+    
 
-    public static Scanner sc = new Scanner(System.in);
+    
+    
+	public undertale game;
+	public dialogue(undertale e){
+		
+		 game = e;
+	}
+	
+	public dialogue(){
+		try {
+			dialogueBox = ImageIO.read(new File("assets/scripts/dialogueBox.png"));
+		    font = Font.createFont(Font.TRUETYPE_FONT, new File("assets/fonts/DTM-Sans.ttf")).deriveFont(30f);
 
-    public static BufferedImage dialogueBox;
-    public static Font font;
+		    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		    //register the font
+		    ge.registerFont(font);
+		    
+		} catch (IOException | FontFormatException e) {
+			// TODO Auto-generated catch block
+			System.out.println("image does not exist");
+			e.printStackTrace();
+		}
+		setPreferredSize(new Dimension(990, 615));
+		setBackground(new Color(0, 0, 0));
+		addKeyListener(this);
+		this.setFocusable(true);
+		
+		
+		// import scripts
+		for (int i = 1; i < 4; i++) {
+			for (int x = 1; x < 5; x++) {
+			
+				try {
+					allScripts[i][x] = new ArrayList<String>();
+					currentFile = new Scanner(new File("assets/scripts/" + i + "_" + x + "_script.txt"));
+					
+					// put all lines of the text-file into an ArrayList that holds strings
+					while (currentFile.hasNextLine()){
+						allScripts[i][x].add(currentFile.nextLine());
+					}
+				} 
+				catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block 
+				}
 
-    // max letters = 54
+			}
+		}
+	}
+	
+	
+	/*
+	 * The paint method component that will be implemented in the main file
+	 * It draws the dialogue box and then 3 lines starting from the start line.
+	 * The start line is an index to track which portion of the script we are on.
+	 * 
+	 */
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		if (speaking) {
+	   		g.setColor(new Color(255, 255, 255));
+			g.setFont(font);
+			g.drawImage(dialogueBox, 40, 420, null);
+			for (int i = 0; i < 3; i++) {
+				if (i + startLine < allScripts[1][1].size()) {
+					g.drawString(allScripts[1][1].get(i + startLine), 300, 485 + 40 * i);
+				}
+				else {
+					startLine = 0;
+					speaking = false;
+					break;
+				}
+			}
+		}
 
-    public static boolean speaking = true;
-    public static Scanner currentFile;
-    public static ArrayList<String>[][] allScripts = new ArrayList[4][5];
-    public static int startLine = 0;
-    public static int maxLines = 0;
+	}
+	public static void main(String[] args) throws IOException{
+		JFrame frame = new JFrame("UNDERTALE");
+		dialogue panel = new dialogue();
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setResizable(false);
+	}
+	
+	
+	/*
+	 * keyPressed() will check if the player presses 'Z' or 'z' to 
+	 * scroll through the dialogue. It adds 3 to start line since 
+	 * we want to skip 3 lines after we displayed them
+	 * The following if statements below are just for heals or if 
+	 * the player has reached the trophy at the end of the game.
+	 */
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		if (speaking) {
+			if (e.getKeyChar() == 'z' || e.getKeyChar() == 'Z') {
+				if (undertale.grabbedHeal) {
+					speaking = false;
+					undertale.grabbedHeal = false;
 
+					
+				}
+				else if (undertale.ended) {
+					undertale.ended = false;
+					speaking = false;
+				}
+				startLine += 3;
+			}
+			System.out.println(startLine);
+		}
+		repaint();
 
+	}
 
-
-    public undertale game;
-    public dialogue(undertale e){
-
-        game = e;
-    }
-
-    public dialogue(){
-        try {
-            dialogueBox = ImageIO.read(new File("assets/scripts/dialogueBox.png"));
-            font = Font.createFont(Font.TRUETYPE_FONT, new File("assets/fonts/DTM-Sans.ttf")).deriveFont(30f);
-
-            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-            //register the font
-            ge.registerFont(font);
-
-        } catch (IOException | FontFormatException e) {
-            // TODO Auto-generated catch block
-            System.out.println("image does not exist");
-            e.printStackTrace();
-        }
-        setPreferredSize(new Dimension(990, 615));
-        setBackground(new Color(0, 0, 0));
-        addKeyListener(this);
-        this.setFocusable(true);
-        // import scripts
-
-        for (int i = 1; i < 4; i++) {
-            for (int x = 1; x < 5; x++) {
-
-                try {
-                    allScripts[i][x] = new ArrayList<String>();
-                    currentFile = new Scanner(new File("assets/scripts/" + i + "_" + x + "_script.txt"));
-                    while (currentFile.hasNextLine()){
-                        allScripts[i][x].add(currentFile.nextLine());
-                    }
-                }
-                catch (FileNotFoundException e) {
-                    // TODO Auto-generated catch block
-                }
-
-            }
-        }
-    }
-
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        if (speaking) {
-            g.setColor(new Color(255, 255, 255));
-            g.setFont(font);
-            g.drawImage(dialogueBox, 40, 420, null);
-            for (int i = 0; i < 3; i++) {
-                if (i + startLine < allScripts[1][1].size()) {
-                    g.drawString(allScripts[1][1].get(i + startLine), 300, 485 + 40 * i);
-                }
-                else {
-                    startLine = 0;
-                    speaking = false;
-                    break;
-                }
-            }
-        }
-
-    }
-    public static void main(String[] args) throws IOException{
-        JFrame frame = new JFrame("UNDERTALE");
-        dialogue panel = new dialogue();
-        frame.add(panel);
-        frame.pack();
-        frame.setVisible(true);
-        frame.setResizable(false);
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        // TODO Auto-generated method stub
-
-        if (speaking) {
-            if (e.getKeyChar() == 'z' || e.getKeyChar() == 'Z') {
-                if (undertale.grabbedHeal) {
-                    speaking = false;
-                    undertale.grabbedHeal = false;
-
-
-                }
-                else if (undertale.ended) {
-                    undertale.ended = false;
-                    speaking = false;
-                }
-                startLine += 3;
-            }
-            System.out.println(startLine);
-        }
-        repaint();
-
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // TODO Auto-generated method stub
-    }
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
 
 
 
-    @Override
-    public void keyReleased(KeyEvent e) {
-        // TODO Auto-generated method stub
-
-    }
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
